a3f5ab4a314631062085f4ce52c27595670ece07
Supercip971 <supercyp971@gmail.com>
2023-08-06 12:47:09 -0400

    feat: add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,34 @@
 </script>
 
 <slot />
+
+<footer class="flex space-x-16 w-fit mx-auto py-8">
+  <div class="">
+    <h4 class="text-center">Contact</h4>
+    <p class="text-center space-x-8">
+      <a href="mailto:contact@cute.engineering">
+        mail 
+      </a> 
+      <a href="https://twitter.com/cute_engineering">
+        twitter
+      </a>
+    </p>
+  </div>
+
+  <div class="text-center">
+    <h4 class="text-center">Source</h4>
+    <p>
+      <a href="https://github.com/cute-engineering/website">
+        github
+      </a>
+    </p>
+  </div>
+  <div class="text-center">
+    <h4 class="text-center">License</h4>
+    <p>
+      <a href="https://github.com/cute-engineering/website/license">
+        mit license
+      </a>
+    </p>
+  </div>
+</footer>
