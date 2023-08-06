6380e66c83dd24ba1262c257b836991acb55b3b6
Supercip971 <supercyp971@gmail.com>
2023-08-06 11:58:31 -0400

    page: add a 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<main>
+    <section class="h-screen flex flex-col justify-around text-center ">
+        <div>
+            <img class="mx-auto" src="/hat.svg" width="200px" height="200px" alt="hat" />
+            <h1 class="">Error</h1>
+            <p class="py-4">{$page.status}: {$page.error.message}</p>
+        </div>
+        <h2><a href="/" class="text-center w-fit">Home</a></h2>
+    </section>
+</main>
+    
